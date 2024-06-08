b05a92fbff0698caf9f85f1e8ed763ba6baac868
SonJinHYo <wlsgy95@naver.com>
2024-06-08 18:52:21 +0900

    Fix: 잘못된 alembic 마이그레이션 파일 수정
    - mysql 기존 데이터를 모두 드랍하는 코드 제거

---
+++ alembic/versions/f82d979b7370_initial_migration.py
@@ -0,0 +1,26 @@
+"""Initial migration
+
+Revision ID: f82d979b7370
+Revises: 
+Create Date: 2024-06-08 18:02:12.922277
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import mysql
+
+# revision identifiers, used by Alembic.
+revision: str = 'f82d979b7370'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
